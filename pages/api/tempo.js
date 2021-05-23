599707b8c34aa8c26d4b8a347f21a57adf26feeb
Gabriel Basilo <gabrieldev4@gmail.com>
2021-05-23 03:11:36 -0300

    adicionar Cache

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -5,6 +5,9 @@ async function tempo (req,res){
     const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`);
     const subscribersResponseJson = await subscribersResponse.json();
     const inscritos = subscribersResponseJson.total_subscribers;
+
+    res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
+    
     res.json({
         date: dynamicData.toGMTString(),
         inscritos: inscritos
